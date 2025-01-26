6c8aa6d783342ad32ea0867236402d878ccec890
AzamatUzakov <azamatakmal053@gmail.com>
2025-01-26 17:27:43 +0500

    day 2 study

---
+++ day-2.js
@@ -0,0 +1,9 @@
+
+
+
+let numbers = [10, 15, 20, 25, 30, 35];
+
+let newNums = numbers.filter(num => num % 2 === 0);
+console.log(newNums);
+
+
--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 //Алгоритм для уникальных символов строки
-function Uniquest(str) {
+
+function Unique(str) {
 
     for (let i = 0; i < str.length; i++) {
-        for (let j = i + 1; j < str.length; j++)
+
+        for (let j = i + 1; j < str.length; j++) {
 
             if (str[i] === str[j]) {
-                return false;
-
+                return false
             }
 
-
+        }
     }
     return true;
 }
 
-console.log(Uniquest('abcd'));
-console.log(Uniquest('aabcd'));
-
-console.log("hello");
-
+console.log(Unique("abcd"));
+console.log(Unique("Hello"));
