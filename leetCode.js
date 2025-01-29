80aff9a249bb6d9f857faf01e3a8a17a5faeb596
AzamatUzakov <azamatakmal053@gmail.com>
2025-01-29 16:50:51 +0500

    task-2 Счетчик

--- leetCode.js
+++ leetCode.js
@@ -1,4 +1,19 @@
+
+//task-1 Массив Прототип Последний
 Array.prototype.last = function() {
     return this.length === 0 ? -1 : this[this.length-1]
 
 };
+
+
+/// task-2 Счетчик
+var createCounter = function (n) {
+    return function() {
+        return n++;
+    };
+
+};
+//const counter = createCounter(10);
+console.log(createCounter()); // 10
+console.log(createCounter()); // 11
+console.log(createCounter()); // 12
